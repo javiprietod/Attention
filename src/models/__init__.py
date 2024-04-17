08a50002223d1d4609983ad9db84856e1bd61b94
Alex <alex.m.guinea@alu.comillas.edu>
2024-04-17 16:28:26 +0200

    [FIX] - Merge with develop fixed (imported PytorchModel in __init__.py).

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,2 @@
-from src.models.base import EncoderModel, SelfAttention, PositionalEncoding
+from src.models.base import EncoderModel, SelfAttention, PositionalEncoding, PytorchModel
 from src.models.kernelized_model import KernelizedModel, KernelizedAttention
